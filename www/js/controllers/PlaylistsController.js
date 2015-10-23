a5d0363c81e091c365d13471fd4918a98b872e3b
Fatima Grace Carillo <fatima.g.m.carillo@accenture.com>
2015-10-23 13:54:25 +0800

    initial commit

---
+++ www/js/controllers/PlaylistsController.js
@@ -0,0 +1,19 @@
+
+var playlists = [
+		{ title: 'Reggae', id: 1 },
+		{ title: 'Chill', id: 2 },
+		{ title: 'Dubstep', id: 3 },
+		{ title: 'Indie', id: 4 },
+		{ title: 'Rap', id: 5 },
+		{ title: 'Cowbell', id: 6 }
+	];
+
+app.controller('PlaylistsController', function($scope, $stateParams) {
+	// details
+	// if($stateParams){
+		
+	// }
+	// else
+		$scope.playlists = playlists;
+
+})
